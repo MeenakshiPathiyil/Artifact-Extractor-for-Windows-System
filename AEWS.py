ecc76ce77d45350e98e52e546d474464cf69a527
Meenu2019 <54184349+Meenu2019@users.noreply.github.com>
2023-08-21 12:27:06 +0530

    add system info

--- AEWS.py
+++ AEWS.py
@@ -55,9 +55,13 @@ def get_services_as_dict():
 services = get_services_as_dict()
 df2 = pd.DataFrame(services)
 
+
 # Get information about system memory usage
 memory = psutil.virtual_memory()
 memory_used_gb = memory.used / (1024 ** 3)
+total_memory_gb = memory.total / (1024 ** 3)
+available_memory_gb = memory.available / (1024 ** 3)
+memory_perc = memory.percent
 
 # Get information about logged-in users
 user = psutil.users()
@@ -68,6 +72,7 @@ usage_disk = psutil.disk_usage('/')
 total_gb = usage_disk.total / (1024 ** 3)
 used_gb = usage_disk.used / (1024 ** 3)
 free_gb = usage_disk.free / (1024 ** 3)
+disk_perc = usage_disk.percent
 
 # Write process data to CSV files
 df.to_csv('processes.csv', index=False)
@@ -83,4 +88,8 @@ with open('sys_info.txt', 'w') as f:
     f.write('Total space in disk = ' + str(total_gb) + '\n')
     f.write('Used space in disk = ' + str(used_gb) + '\n')
     f.write('Free space in disk = ' + str(free_gb) + '\n')
-    f.write('Memory used = ' + str(memory_used_gb))
+    f.write('Disk Usage Percent = ' + str(disk_perc) + '\n')
+    f.write('Total Memory = ' + str(total_memory_gb) + '\n')
+    f.write('Memory used = ' + str(memory_used_gb) + '\n')
+    f.write('Available Memory = ' + str(available_memory_gb) + '\n')
+    f.write('Memory Percent = ' + str(memory_perc))
